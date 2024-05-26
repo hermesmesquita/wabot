b02b35efe895c7c70996da9700f008154fe1fff8
Hermes Mesquita <breuwebber@gmail.com>
2024-05-25 22:06:10 -0300

    Atualização do texto-resposta

--- wabot.js
+++ wabot.js
@@ -7,11 +7,7 @@ wppconnect.create({
             })
     .then(client => client.onMessage( message =>{
                     console.log('\n Mensagem digitada pelo usuário:\n\t' + message.body)
-<<<<<<< HEAD
-                    client.sendText(message.from, 'Ignore esta mensagem mais ainda. \n\t')
-=======
                     client.sendText(message.from, 'Ignore esta mensagem mais uma vez. \n\t')
->>>>>>> a97ca02c0741f02ab82a1b605065e241b7adc01c
                         .then( result => console.log('Mensagem retornado: \n\t' + result.body))
                         .catch( erro => console.error(erro))
                     }))
