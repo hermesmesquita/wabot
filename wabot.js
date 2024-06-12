868012e8395d3fea6e2d4588b6e117cbae64ea3c
Hermes Mesquita <hermesmesquita@gmail.com>
2024-06-12 00:21:45 +0000

    Perfumaria

--- wabot.js
+++ wabot.js
@@ -1,17 +1,16 @@
 const wppconnect = require('@wppconnect-team/wppconnect')
 
 wppconnect.create({
-    session: 'wabot', 
-    autoClose: false,
-    puppeteerOptions: {args: ['--no-sandbox']}
+        session: 'wabot', 
+        autoClose: false,
+        puppeteerOptions: {args: ['--no-sandbox']}
 })
-.then(client => client
-    .onMessage( message =>{
-        console.log(new Date(Date.now()) + 'Usuário digitou:\n\t' + message.body)
-        client
-            .sendText( message.from, "🤖Robô em treinamento. Ignore a mensagem.")
-            .then( result => console.log('Mensagem retornada:\n\t' + result.body))
-            .catch( erro => console.error(erro))
+   .then(client => client
+        .onMessage( message =>{
+            console.log(new Date(Date.now()) + 'Usuário digitou:\n\t' + message.body)
+            client
+                .sendText( message.from, "🤖Robô em treinamento. Ignore a mensagem.")
+                .then( result => console.log('Mensagem retornada:\n\t' + result.body))
+                .catch( erro => console.error(erro))
     }))
-    .catch( erro => console.error(erro))
-
+        .catch( erro => console.error(erro))
